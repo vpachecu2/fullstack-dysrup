faa01a71b665fa5060dc2b40324a68fffa332b1d
Victor Pacheco <vpachecu2@gmail.com>
2025-07-17 15:09:34 -0300

    feat: create database connection

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
+console.log(process.env.DB_HOST);
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // garante acesso em todo o app
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST ?? 'localhost',
@@ -14,6 +20,8 @@ import { AppService } from './app.service';
       database: process.env.DB_NAME ?? 'projectapp',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
+      autoLoadEntities: true,
+      logging: true,
     }),
   ],
   controllers: [AppController],
